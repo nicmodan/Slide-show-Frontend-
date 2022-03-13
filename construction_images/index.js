a65bd3c58c64717369e0830297cfb4de597cc689
Michael Awofeko <michaelawofeko@gmail.com>
2022-03-13 19:05:46 +0100

    First Commit

---
+++ construction_images/index.js
@@ -0,0 +1,17 @@
+import contact from '../../../Asserts/SlideImages/construction_images/contact.jpg'
+import hsevnp from '../../../Asserts/SlideImages/construction_images/health_safety_environment_policies.jpg'
+import inquiry from '../../../Asserts/SlideImages/construction_images/Inquiry.jpg'
+import projects from '../../../Asserts/SlideImages/construction_images/projects.jpg'
+import services from '../../../Asserts/SlideImages/construction_images/services.jpg'
+import yance from '../../../Asserts/SlideImages/construction_images/yance.jpg'
+
+const Images = {
+    contact,
+    hsevnp,
+    inquiry,
+    projects,
+    services,
+    yance
+}
+
+export default Images
